335fcacbc6045259ff97a8a087ce6367d369e5de
koka <koka.code@gmail.com>
2018-08-18 16:41:18 +0900

    refac

--- shrc/00_export.sh
+++ shrc/00_export.sh
@@ -1,44 +1,47 @@
-
 export PATH="/usr/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export PATH="~/dotfiles/etc/scripts:$PATH"
 export PATH="$DOTPATH/bin:$PATH"
-# for gnu replace
-export PATH="/opt/gradle/gradle-3.4.1/bin:$PATH"
+
 #neovim
 export XDG_CONFIG_HOME="$HOME/.config"
-export LD_LIBRARY_PATH=$(rustc --print sysroot)/lib:$LD_LIBRARY_PATH
-export BAT_CONFIG_DIR=$(bat cache --config-dir)
+
 #nodeblew
 # export PATH=$HOME/.nodebrew/current/bin:$PATH
-# for kubernetes
+
+##########################################
+# for virtual env
+##########################################
+# k8s
 export MINIKUBE_HOME=$HOME
 export KUBECONFIG=$HOME/.kube/config
-# anyenv
-#export PATH="$HOME/.anyenv/bin:$PATH"
-# export PATH="$HOME/.exenv/bin:$PATH"
-# eval "$(anyenv init -)"
-# eval "$(exenv init -)"
-# sqlite
+# scala
 export PATH="$HOME/.scalaenv/bin:$PATH"
 eval "$(scalaenv init -)"
+export PATH="/opt/gradle/gradle-3.4.1/bin:$PATH"
+# ruby
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-export PATH="/usr/local/opt/sqlite/bin:$PATH"
-# codeprep
-export CODEPREP_BACKEND_PATH=$HOME/Documents/src/givery/codeprep/codeprep-backend
-# curl
-export PATH="/usr/local/opt/curl/bin:$PATH"
-# gopath
-#export GOROOT=/usr/local/opt/go/libexec
+# python
+eval "$(pipenv --completion)"
+export PIPENV_VENV_IN_PROJECT=true
+# golang
 export GOPATH=$HOME/src/github/go
 export GOBIN=$GOPATH/bin
 export PATH=$GOPATH/bin:$PATH
 export PATH="$HOME/.cargo/bin:$PATH"
+# rust
 export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
-#export PATH="~/Documents/src/go/bin"
+export LD_LIBRARY_PATH=$(rustc --print sysroot)/lib:$LD_LIBRARY_PATH
+
+##########################################
+# additional commands
+##########################################
+# bat https://github.com/sharkdp/bat
+export BAT_CONFIG_DIR=$(bat cache --config-dir)
+
 #git::diff-highright
 export PAGER=less
 export LESS='-i -M -R'
@@ -51,9 +54,3 @@ export LESS_TERMCAP_ue=$'\E[0m'          # Ends underline.
 export LESS_TERMCAP_us=$'\E[01;32m'      # Begins underline.
 
 export MDV_CODE_THEME=all
-
-## export mycommand
-export PATH="$HOME/src/givery/codeprep/codeprep_contents_generator:$PATH"
-export CODEPREP_TEMPLATE_PATH="$HOME/src/givery/codeprep/codeprep_contents_generator"
-## e
-
